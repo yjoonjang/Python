b47caf08f0b004f11d33ac0b70a466d1fe97e3d9
yjoonjang <82500463+yjoonjang@users.noreply.github.com>
2021-08-03 01:37:53 +0900

    Update number_baseball_game.py
    - 중복 입력 시 다시 입력하도록 유도
    - elif 굳이 2번 필요없고, 하나만 쓴 후 나머지 하나는 else 가 되도록 수정

--- number_baseball_game.py
+++ number_baseball_game.py
@@ -1,18 +1,28 @@
 answer = input("0~9 사이의 숫자를 연속으로 입력하시오 (중복입력 금지) :")
+for i in range(len(answer)):
+    if answer[i] in answer[i+1:]:
+        print("중복입력하셨습니다. 다시 입력해 주세요")
+        break;
 predict = input("예상되는 0~9 사이의 숫자를 연속으로 입력하시오 (중복입력 금지):")
-strike = 0
-ball = 0
-out = 0
-
+for i in range(len(predict)):
+    if predict[i] in predict[i+1:]:
+        print("중복입력하셨습니다. 다시 입력해 주세요")
+        break;
+    else: 
+        strike = 0
+        ball = 0
+        out = 0
 for i in range(len(answer)):
     if answer[i] == predict[i]:
         strike += 1
     elif predict[i] in answer:
         ball += 1
-    elif predict[i] not in answer:
+    else:
         out += 1
 
 if out == len(answer):
     print("OUT")
 else:
     print("{0}STRIKE {1}BALL".format(strike, ball))
+
+
